01f6f0d20d69400962e1a0e30a9d2fb6d1bf19dc
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2023-10-06 23:43:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/composabellayout_pertemuan3/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.composabellayout_pertemuan3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.R
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composabellayout_pertemuan3.ui.theme.Composabellayout_pertemuan3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Composabellayout_pertemuan3Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Composabellayout_pertemuan3Theme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun TampilText(message:String, from:String, modifier: Modifier=Modifier) {
+    Column(verticalArrangement = Arrangement.Center,
+        modifier = modifier){
+        Text(
+            color = Color.Blue,
+            text = message,
+            fontSize = 80.sp,
+            lineHeight = 110.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            color = Color.Red,
+            text = from,
+            fontSize = 60.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun TampilLayar(pesan: String, dari: String) {
+    val image = painterResource(com.example.composabellayout_pertemuan3.R.drawable.dorabrill)
+    Box{
+        Image(painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier.fillMaxSize())
+        TampilText(message = pesan,
+            from = dari,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  Composabellayout_pertemuan3Theme  {
+        TampilLayar(pesan = " Hi Brilly Cantik", dari = " Dari Doraemon Mu")
+        }
+}
